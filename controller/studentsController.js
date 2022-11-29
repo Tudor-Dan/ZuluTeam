5ca9a025dc64272eefa6d6c17710ea9dc86cffc4
claudiumihait <claudium.tudor@gmail.com>
2022-11-29 11:39:14 +0200

    ticket 3

--- controller/studentsController.js
+++ controller/studentsController.js
@@ -2,43 +2,71 @@ const display = require("../view/display");
 const dataManager = require("../model/dataManager");
 
 const getAllStudents = () => {
-    const students = dataManager.readData("data.json");
-    display.printData(students, "Students Table:");
-}
+  const students = dataManager.readData("data.json");
+  display.printData(students, "Students Table:");
+};
+
+const getNumberOfSchoolClasses = () => {
+  const students = dataManager.readData("data.json");
+  const classes = [];
+  students.forEach((s) =>
+    Object.keys(s).forEach((key) =>
+      key.includes("subject") && !key.includes("Mark")
+        ? !classes.includes(s[`${key}`])
+          ? classes.push(s[`${key}`])
+          : null
+        : null
+    )
+  );
+  display.printMessage(
+    `There is a total of ${classes.length} school classes`,
+    false
+  );
+};
+
 
 const hasChosen = () => {
-    const userOption = display.getInput("Please enter a number: ");
-    if (userOption === "1") {
-        getAllStudents();
-    } else if (userOption === "2") {
-        display.printMessage("'Add student' not implemented yet.", true);
-    } else if (userOption === "3") {
-        display.printMessage("'Update student' not implemented yet.", true);
-    } else if (userOption === "4") {
-        display.printMessage("'Delete student' not implemented yet.", true);
-    } else if (userOption === "0") {
-        return false;
-    } else {
-        display.printMessage("There is no such option.", true);
-    }
-    return true;
-}
+  const userOption = display.getInput("Please enter a number: ");
+  if (userOption === "1") {
+    getAllStudents();
+  } else if (userOption === "2") {
+    display.printMessage("'Add student' not implemented yet.", true);
+  } else if (userOption === "3") {
+    display.printMessage("'Update student' not implemented yet.", true);
+  } else if (userOption === "4") {
+    display.printMessage("'Delete student' not implemented yet.", true);
+  } else if (userOption === "5") {
+    getNumberOfSchoolClasses();
+  } else if (userOption === "0") {
+    return false;
+  } else {
+    display.printMessage("There is no such option.", true);
+  }
+  return true;
+};
 
 const handleSubmenu = () => {
-    const optionsArray = ["Exit submenu", "List students", "Add a new student", "Update student", "Delete student"];
-    display.printMenu("Student Classes Submenu", optionsArray);
-}
+  const optionsArray = [
+    "Exit submenu",
+    "List students",
+    "Add a new student",
+    "Update student",
+    "Delete student",
+    "Get number of school classes",
+  ];
+  display.printMenu("Student Classes Submenu", optionsArray);
+};
 
 const submenu = () => {
-    let isRunning = true;
-    while (isRunning) {
-        handleSubmenu();
-        try {
-            isRunning = hasChosen();
-        } catch (error) {
-            display.printMessage(error, true);
-        }
+  let isRunning = true;
+  while (isRunning) {
+    handleSubmenu();
+    try {
+      isRunning = hasChosen();
+    } catch (error) {
+      display.printMessage(error, true);
     }
-}
+  }
+};
 
-module.exports = {submenu};
+module.exports = { submenu };
